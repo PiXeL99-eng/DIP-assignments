518f22bd2a7bb6ae13c6f54367afd34cbcfa1bfd
Sayantan Kundu <sayantankundu02@gmail.com>
2024-02-18 19:59:24 +0530

    completed assignment 1

--- Assignment 1/q6.py
+++ Assignment 1/q6.py
@@ -1,5 +1,46 @@
 import cv2
 
+def rotate_image(image_path, rotation_angle):
+    
+    # Read the image and return a numpy array loaded with the image
+    img = cv2.imread(image_path)
+    cv2.imshow('Original Image', img)    # window_name, image
+    cv2.waitKey(0)
+
+    height, width = img.shape[:2]
+
+    # Calculate the rotation matrix and rotate image
+    rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), rotation_angle, 1)   # (center, angle, scale) returns rotated matrix
+    rotated_img = cv2.warpAffine(img, rotation_matrix, (width, height))     # (input_image, transformation_matrix, size_of_output_img)
+
+    cv2.imshow('Rotated Image', rotated_img)    # window_name, image
+    cv2.waitKey(0)
+
+    # Close all windows
+    cv2.destroyAllWindows()
+
+def check_intensity_class(image_path):
+    
+    # Read the image and return a numpy array loaded with the image
+    img = cv2.imread(image_path)
+    print("Intensity class:", type(img[0][0][0]))
+
+def convert_to_double(image_path):
+    
+    # Read the image and return a numpy array loaded with the image
+    img = cv2.imread(image_path)
+    cv2.imshow('Original Image', img)    # window_name, image
+    cv2.waitKey(0)
+
+    img_double = img.astype('float64') / 255.0
+    cv2.imshow('Converted image', img_double)    # window_name, image
+    cv2.waitKey(0)
+
+    print("Intensity class:", type(img_double[0][0][0]))
+
+    # Close all windows
+    cv2.destroyAllWindows()
+
 def convert_to_binary(image_path):
 
     # Read the image and return a numpy array loaded with the image
@@ -14,29 +55,12 @@ def convert_to_binary(image_path):
     # Close all windows
     cv2.destroyAllWindows()
 
-def rotate_image(image_path, rotation_angle):
-    
-    # Read the image and return a numpy array loaded with the image
-    img = cv2.imread(image_path)
-    cv2.imshow('Original Image', img)    # window_name, image
-    cv2.waitKey(0)
-
-    height, width = img.shape[:2]
-
-    # Calculate the rotation matrix and rotate image
-    rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), rotation_angle, 1)
-    rotated_img = cv2.warpAffine(img, rotation_matrix, (width, height))
-
-    cv2.imshow('Rotated Image', rotated_img)    # window_name, image
-    cv2.waitKey(0)
-
-    # Close all windows
-    cv2.destroyAllWindows()
-
 # Image path
 image_path = r'C:\Users\sayan\Desktop\ETCE Eight Sem - Shortcut\DIP\DIP-assignments\Assignment 1\waterlilies.jpg'
 rotation_angle = 45
 
 # Call the function
 rotate_image(image_path, rotation_angle)
+check_intensity_class(image_path)
+convert_to_double(image_path)
 convert_to_binary(image_path)
